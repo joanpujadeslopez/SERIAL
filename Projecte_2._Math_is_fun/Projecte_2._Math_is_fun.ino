fc88f9bea0ee002b35bec2d9ec67d393a6052ea7
joanpujadeslopez <jpl8332@gmail.com>
2016-02-12 19:57:32 +0100

    Math is fun
    2.1

---
+++ Projecte_2._Math_is_fun/Projecte_2._Math_is_fun.ino
@@ -0,0 +1,55 @@
+/***************************************************************
+**                                                            **
+**  Operacions de suma, resta, multiplicació, porcentatge i   **
+**                          divisió                           **                             
+**                                                            **
+***************************************************************/
+
+//******  Includes  ********************************************
+int a=5;
+int b=10;
+int c=20;
+
+//******  Variables  *******************************************
+
+
+//******  Setup  ***********************************************
+void setup(){
+  Serial.begin(9600); //inicia la llibrería Serial a 9600bps
+  Serial.println("Here is some math: ");
+
+  Serial.print("a = ");
+  Serial.println(a);
+  Serial.print("b = ");
+  Serial.println(b);
+  Serial.print("c = ");
+  Serial.println(c);
+
+  Serial.print("a + b = ");       // suma
+  Serial.println(a + b);
+  Serial.print("a * c = ");       // multiplicació
+  Serial.println(a * c);
+  
+  Serial.print("c / b = ");       // divisió
+  Serial.println(c / b);
+
+  Serial.print("c % b = ");       // porcentatge entre c i b
+  Serial.println(c % b);
+  
+  Serial.print("b - c = ");       // resta
+  Serial.println(b - c);
+}
+
+void loop()  // we need this to be here even though its emptys'ha de escriure encara que no ho fem servir
+{
+}
+
+
+
+
+
+
+
+
+
+ 
