155e6768787112147193752aff890771a8ebd6ee
joanpujadeslopez <jpl8332@gmail.com>
2016-02-15 16:09:22 +0100

    Hipotenusa

--- _2.2/_2.2.ino
+++ _2.2/_2.2.ino
@@ -10,17 +10,17 @@
 //******  Variables  *******************************************
 int a = 3;
 int b = 4;
-int h;
+
 
 //******  Setup  ***********************************************
 void setup(){
-   Serial.begin(9600); //inicia la llibrería Serial a 9600bps
-   Serial.print("a = ");
+  Serial.begin(9600); //inicia la llibrería Serial a 9600bps
+  Serial.print("a = ");
   Serial.println(a);
   Serial.print("b = ");
   Serial.println(b);
   Serial.print("h = ");
-  Serial.print sqrt(pow(a,2)+ pow(b,2));  // Arrel quadrada de a al cuadrat mes b al cuadrat
+  Serial.print (sqrt(pow(a,2)+ pow(b,2)));  // Arrel quadrada de a al cuadrat mes b al cuadrat
   
 }
 
